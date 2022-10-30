90a600747ab60cd7e1d4df04a58e481cb3ab27e0
Marilynn Stone <hello@marilynnstone.com>
2022-10-29 20:12:17 -0700

    Move player creation to main.rb.

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
-require 'player'
-require 'questions'
-require 'game'
+require './player'
+#require './questions'
+#require './game'
 
-class Main
-  def initialize()
-  end
-end
+@players = []
+
+puts "Player 1 what is your name?"
+player_1_name = gets.chomp
+puts "Player 2 what is your name?"
+player_2_name = gets.chomp
+
+player_1 = Player.new(player_1_name)
+@players << player_1
+player_2 = Player.new(player_2_name)
+@players << player_2
+
--- player.rb
+++ player.rb
@@ -7,12 +7,7 @@ class Player
     @score = score
   end
 
+  
+
 end
 
-  puts "Player 1 what is your name?"
-  player_1_name = gets.chomp
-  puts "Player 2 what is your name?"
-  player_2_name = gets.chomp
-
-  player_1 = Player.new(player_1_name)
-  player_2 = Player.new(player_2_name)
