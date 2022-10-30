54b9309ce23a13d5d581417910082a4f785fa2b7
Marilynn Stone <hello@marilynnstone.com>
2022-10-29 18:42:03 -0700

    Begin creating game loop.

--- game.rb
+++ game.rb
@@ -1,5 +1,11 @@
 class Game
-  def initialize()
-
+  def initialize(player_score, current_player)
+    @current_player = current_player
+    @player_score = player_score
   end
+
+  @current_player = player_1
+
+  @current_player = player_1 ? puts "#{player_1.name}, it's your turn." : puts "#{player_2.name}, it's your turn."
+  
 end
