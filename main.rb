ea0f3427bcd378a93bf6abfd90a2519f73772356
Marilynn Stone <hello@marilynnstone.com>
2022-10-30 14:53:26 -0700

    Add comments.

--- main.rb
+++ main.rb
@@ -2,15 +2,19 @@ require './player'
 #require './questions'
 #require './game'
 
+# Store new game player info.
 @players = []
 
+# Get player name input.
 puts "Player 1 what is your name?"
 player_1_name = gets.chomp
 puts "Player 2 what is your name?"
 player_2_name = gets.chomp
 
+# Create new player instances and add them to the @players array.
 player_1 = Player.new(player_1_name)
 @players << player_1
 player_2 = Player.new(player_2_name)
 @players << player_2
 
+# Start a new game.
